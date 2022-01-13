ebadd4be448ef4b457b304fe25074400574450d5
Anthony Jimenez <tony6776a@gmail.com>
2022-01-13 15:52:44 -0500

    testing weatherboard api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
+function getWeather(){
+    if(city == "union")
+    fetch(weathboardAPI)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        
+    })
+}
+
 var selectedEl = $("#search-city");
 var cityChosen = document.getElementById("current-city");
 var temp = document.getElementById("temperature");
 var humidity = document.getElementById("humidity");
 var windSpeed = document.getElementById("wind-speed");
 var uvIndex = document.getElementById("uv");
-
 var cityArray = [];
-var city = "";
 
-var APIKey = "3c3fe44616a0a33f54223a0fa29fe9b2";
+var weatherboardApiLink = "api.openweathermap.org/data/2.5"
+var city = "union"
+var APIKey = "&appid=3c3fe44616a0a33f54223a0fa29fe9b2";
+var weathboardAPI = weatherboardApiLink + city + APIkey;
