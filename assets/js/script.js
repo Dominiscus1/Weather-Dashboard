5c848691243a3ce5de9191e1dea6af02d3cd295d
Anthony Jimenez <tony6776a@gmail.com>
2022-01-13 15:29:03 -0500

    Created index js and css and started working with weathboard api

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var selectedEl = $("#search-city");
+var cityChosen = document.getElementById("current-city");
+var temp = document.getElementById("temperature");
+var humidity = document.getElementById("humidity");
+var windSpeed = document.getElementById("wind-speed");
+var uvIndex = document.getElementById("uv");
+
+var cityArray = [];
+var city = "";
+
+var APIKey = "3c3fe44616a0a33f54223a0fa29fe9b2";
